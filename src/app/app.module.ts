d8573073b63972fe9b45c7e85727258ae0bfc295
Mahasooq AT <mahasooq.me@gmail.com>
2020-02-17 15:04:46 +0530

    Complete listing the todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
@@ -9,7 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule, NgbModule
+    BrowserModule, NgbModule, FormsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
