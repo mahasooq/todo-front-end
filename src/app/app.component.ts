50b6864ab14b99e0637361ed27b0ef597801bda6
Mahasooq AT <mahasooq.me@gmail.com>
2020-02-17 17:02:44 +0530

    Todo creation, styling todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Todo } from './models/todo/todo';
+import { Todo, TodoForCreation } from './models/todo/todo';
 import { RepositoryService } from './services/repository/repository.service';
 
 @Component({
@@ -15,11 +15,23 @@ export class AppComponent {
   ngOnInit() {
     this.getTodos();
   }
+  onSubmit(form: NgForm) {
+    console.log(form.controls['title'].value);
+    let todo = new TodoForCreation(form.controls['title'].value, false);    
+    this.createTodo(todo);
+    form.controls['title'].setValue('');
+  }
   getTodos() {
     this.repository.getData('todo')
     .subscribe(res => {
       let response: any = res;
       this.todoList = response.data as Todo[];
+    })
+  }
+  createTodo(todo: TodoForCreation) {
+    this.repository.create('todo', todo)
+    .subscribe(res => {
+      this.getTodos();
     })
   }
 }
