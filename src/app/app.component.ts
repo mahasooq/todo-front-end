294f20d1ce999bea4ed11b27db10faa69c3c738f
akhilbit <akhil@bititude.com>
2020-02-21 21:42:12 +1100

    added 2 new functions -delete all marked -mark all as completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,51 +11,73 @@ import { RepositoryService } from './services/repository/repository.service';
 export class AppComponent {
   title = 'full-stack-todo-app';
   todoList: Todo[];
-  constructor(private repository: RepositoryService) {}
+  constructor(private repository: RepositoryService) { }
   ngOnInit() {
     this.getTodos();
   }
   onSubmit(form: NgForm) {
     console.log(form.controls['title'].value);
-    let todo = new TodoForCreation(form.controls['title'].value, false);    
+    let todo = new TodoForCreation(form.controls['title'].value, false);
     this.createTodo(todo);
     form.controls['title'].setValue('');
   }
   getTodos() {
     this.repository.getData('todo')
-    .subscribe(res => {
-      this.todoList = res as Todo[];
-    })
+      .subscribe(res => {
+        this.todoList = res as Todo[];
+      })
   }
   createTodo(todo: TodoForCreation) {
     this.repository.create('todo', todo)
-    .subscribe(res => {
-      this.getTodos();
-    })
+      .subscribe(res => {
+        this.getTodos();
+      })
   }
-  setStatus(todo: Todo, event: any){
+  setStatus(todo: Todo, event: any) {
     let field = {
       completed: event.target.checked
     }
 
     this.repository.update(`todo/${todo._id}`, field)
-    .subscribe(res => {
-      this.getTodos();
-    })
+      .subscribe(res => {
+        this.getTodos();
+      })
   }
-  deleteTodo(todo: Todo) {    
+  deleteTodo(todo: Todo) {
     this.repository.delete(`todo/${todo._id}`)
-    .subscribe(res => {
-      this.getTodos();
+      .subscribe(res => {
+        this.getTodos();
+      })
+  }
+
+  markAllAsCompleted() {
+    const updateFields = { completed: true };
+    this.repository.updateAll(`todo/all`, updateFields)
+      .subscribe((res: any) => {
+        console.log({ res })
+        if (res.update)
+          this.todoList.map(todo => {
+            todo.completed = true;
+            return todo;
+          })
+      })
+  }
+
+  deleteAllCompleted() {
+    const condition = { completed: true };
+    this.repository.deleteMany('todo/delete-many', { cond: condition }).subscribe((res: any) => {
+      if (res.deleted)
+        this.todoList = this.todoList.filter(todo => !todo.completed);
     })
   }
-  updateTodo(todo: Todo, event: any) { 
+
+  updateTodo(todo: Todo, event: any) {
     let field = {
       title: event.target.value
     }
     this.repository.update(`todo/${todo._id}`, field)
-    .subscribe(res => {
-      this.getTodos();
-    })
+      .subscribe(res => {
+        this.getTodos();
+      })
   }
 }
--- src/app/services/repository/repository.service.ts
+++ src/app/services/repository/repository.service.ts
@@ -10,26 +10,31 @@ export class RepositoryService {
   public getData = (route: string) => {
     return this.http.get(this.createCompleteRoute(route, environment.urlAddress));
   }
- 
+
   public create = (route: string, body: any) => {
     return this.http.post(this.createCompleteRoute(route, environment.urlAddress), body, this.generateHeaders());
   }
- 
+
   public update = (route: string, body: any) => {
     return this.http.patch(this.createCompleteRoute(route, environment.urlAddress), body, this.generateHeaders());
   }
- 
+
   public delete = (route: string) => {
     return this.http.delete(this.createCompleteRoute(route, environment.urlAddress));
   }
- 
+  public deleteMany = (route: string, cond?: any) => {
+    return this.http.post(this.createCompleteRoute(route, environment.urlAddress), cond);
+  }
+  public updateAll = (route: string, body: any) => {
+    return this.http.patch(this.createCompleteRoute(route, environment.urlAddress), body, this.generateHeaders())
+  }
+
   private createCompleteRoute = (route: string, envAddress: string) => {
     return `${envAddress}/${route}`;
   }
- 
   private generateHeaders = () => {
     return {
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     }
   }
 }
